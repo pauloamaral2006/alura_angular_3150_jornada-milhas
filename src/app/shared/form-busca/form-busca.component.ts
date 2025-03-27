1db5c0a302463f9cb7e17b4a50cc4c6dcb707576
Paulo Amaral <pauloamaral2006@yahoo.com.br>
2025-03-27 14:58:43 -0300

    Aula 03

---
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-busca',
+  standalone: false,
+  templateUrl: './form-busca.component.html',
+  styleUrl: './form-busca.component.scss',
+})
+export class FormBuscaComponent {
+  openDialog() {}
+}
